df2a528968dc5ef087bdd5c01c55cf922370631f
Abdul <javsjobs@gmail.com>
2025-05-14 14:30:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css';
+import React,{useState} from 'react';
+
+function App() {
+  const [one,setone]=useState("")
+  const handleone=(event)=>{
+        setone(parseInt(event.target.value))
+  }
+
+  const [two,settwo]=useState("")
+  const handletwo=(event)=>{
+        settwo(parseInt(event.target.value))
+  }
+
+  const [add,setadd]=useState(0)
+  const handleadd=()=>{
+        setadd(one+two)
+  }
+
+  const handlereset=()=>{
+        setone(0)
+        settwo(0)
+        setadd(0)
+  }
+  return (
+    <>
+      <h1>Sum of 2 Numbers</h1>
+      <div className='container'>
+        <label>Number 1 :</label> <br/>
+        <input type='number' value={one} onChange={handleone} ></input> <br/><br/>
+        <label>Number 2 :</label> <br/>
+        <input type='number' value={two} onChange={handletwo} ></input> <br/><br/>
+        <label>Sum :</label> <br></br>
+        <input type='number' value={add}></input> <br/><br/>
+        <div className='button'><button onClick={handleadd}>Add</button><button onClick={handlereset}>Reset</button></div>
+      </div>
+      
+    </>
+  );
+}
+
+
+export default App;
